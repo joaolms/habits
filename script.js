e7cf310babdb95396e6b73bdc4bcf15fe5414804
João Luiz Messias <joaolmss@gmail.com>
2023-01-22 11:09:38 -0300

    Aula 04

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
 const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup(form)
 
-const data = {
-  run: ["01-01", "01-02", "01-04", "01-05"],
-  water: ["01-02", "01-03", "01-04"],
-  food: ["01-03", "01-04", "01-07"],
+// const data = {
+//   run: ["01-01", "01-02", "01-04", "01-05"],
+//   water: ["01-02", "01-03", "01-04"],
+//   food: ["01-03", "01-04", "01-07"],
+// }
+
+// nlwSetup.setData(data)
+// nlwSetup.load()
+
+const button = document.querySelector("header button")
+
+button.addEventListener("click", add)
+form.addEventListener("change", save)
+
+function add() {
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+  const dayExists = nlwSetup.dayExists(today)
+
+  if (dayExists) {
+    alert("Dia já incluso")
+    return
+  }
+
+  nlwSetup.addDay(today)
 }
+
+function save() {
+  localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
 
 nlwSetup.setData(data)
 nlwSetup.load()
